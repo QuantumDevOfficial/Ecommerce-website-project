e8a30f9ae4f861f75e5444905315b97bcd4128a0
Bedzo Promise <promisebedzo07@gmail.com>
2025-12-29 09:43:20 +0000

    HTML and CSS of the project

---
+++ scripts/product.js
@@ -0,0 +1,375 @@
+const products = [
+    {
+        id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        image: "../images/products/men-black-and-white-graphic-hoodie-plus-joggers-set.jpg",
+        name: "Men Graphic Hoodie plus Joggers - Black & white",
+        priceCedis: "12050",
+        rating: {
+            stars: "4",
+            counts: "300",
+        },
+    },
+
+    {
+        id: "bc2847e9-5323-403f-b7cf-57fde044a955",
+        image: "../images/products/towel-2.jpg",
+        name: "Towels - Variations",
+        priceCedis: "9000",
+        rating: {
+            stars: "4",
+            counts: "310",
+        },
+    },
+
+    {
+        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        image: "../images/products/women-floral-handbag.jpg",
+        name: "Women Floral Handbag",
+        priceCedis: "15000",
+        rating: {
+            stars: "4.5",
+            counts: "423",
+        },
+    },
+
+    {
+        id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+        image: "../images/products/jacket.jpg",
+        name: "Winter Jacket - Black",
+        priceCedis: "11090",
+        rating: {
+            stars: "4.5",
+            counts: "401",
+        },
+    },
+
+    {
+        id: "54e0eccd-8f36-462b-b68a-8182611d9add",
+        image: "../images/products/men-chronograph-watch.jpg",
+        name: "Men Silver Wriste Watch",
+        priceCedis: "8000",
+        rating: {
+            stars: "4",
+            counts: "125",
+        },
+    },
+
+    {
+        id: "3ebe75dc-64d2-4137-8860-1f5a963e534b",
+        image: "../images/products/men-chunky-trail-style-atletic-sneaker.jpg",
+        name: "Men atletic Sneaker",
+        priceCedis: "16000",
+        rating: {
+            stars: "4",
+            counts: "305",
+        },
+    },
+
+    {
+        id: "8c9c52b5-5a19-4bcb-a5d1-158a74287c53",
+        image: "../images/products/women-high-heels-yellow.jpg",
+        name: "Women High Heels - Yellow",
+        priceCedis: "11000",
+        rating: {
+            stars: "4.5",
+            counts: "500",
+        },
+    },
+
+    {
+        id: "dd82ca78-a18b-4e2a-9250-31e67412f98d",
+        image: "../images/products/men-two-tone-black-and-brown-shirt.jpg",
+        name: "Men Shirt - Black & Brown",
+        priceCedis: "25000",
+        rating: {
+            stars: "4.5",
+            counts: "600",
+        },
+    },
+
+    {
+        id: "77919bbe-0e56-475b-adde-4f24dfed3a04",
+        image: "../images/products/men-chunky-white-sneaker.jpg",
+        name: "Men Sneaker - white",
+        priceCedis: "14000",
+        rating: {
+            stars: "4.5",
+            counts: "266",
+        },
+    },
+
+    {
+        id: "3fdfe8d6-9a15-4979-b459-585b0d0545b9",
+        image: "../images/products/men-generic-fashion-sneaker-white.jpg",
+        name: "Men Generic Fashion Sneaker",
+        priceCedis: "19000",
+        rating: {
+            stars: "4",
+            counts: "189",
+        },
+    },
+
+    {
+        id: "58b4fc92-e98c-42aa-8c55-b6b79996769a",
+        image: "../images/products/women-metallic-mini-handbag.jpg",
+        name: "Women Mini Handbag",
+        priceCedis: "12000",
+        rating: {
+            stars: "3.5",
+            counts: "201",
+        },
+    },
+
+    {
+        id: "5968897c-4d27-4872-89f6-5bcb052746d7",
+        image: "../images/products/men-luxury-wristwatch.jpg",
+        name: "Men Luxury Wristwatch",
+        priceCedis: "10000",
+        rating: {
+            stars: "4.5",
+            counts: "290",
+        },
+    },
+
+    {
+        id: "aad29d11-ea98-41ee-9285-b916638cac4a",
+        image: "../images/products/mens-striped-polo-t-shirt.jpg",
+        name: "Men Polo T-shirt",
+        priceCedis: "7000",
+        rating: {
+            stars: "4",
+            counts: "450",
+        },
+    },
+
+    {
+        id: "04701903-bc79-49c6-bc11-1af7e3651358",
+        image: "../images/products/women-off-shoulder-green-dress.jpg",
+        name: "Women Shoulder Green Dress",
+        priceCedis: "16000",
+        rating: {
+            stars: "4.5",
+            counts: "559",
+        },
+    },
+
+    {
+        id: "901eb2ca-386d-432e-82f0-6fb1ee7bf969",
+        image: "../images/products/mens-black-and-gray-low-top-sneaker.jpg",
+        name: "Men Sneaker - Gray & Black",
+        priceCedis: "11050",
+        rating: {
+            stars: "4",
+            counts: "145",
+        },
+    },
+
+    {
+        id: "82bb68d7-ebc9-476a-989c-c78a40ee5cd9",
+        image: "../images/products/mens-black-white-chunky-sport-sneakers.jpg",
+        name: "Men Sport sneaker - Black & white",
+        priceCedis: "13000",
+        rating: {
+            stars: "4",
+            counts: "160",
+        },
+    },
+
+    {
+        id: "c2a82c5e-aff4-435f-9975-517cfaba2ece",
+        image: "../images/products/mens-black-luxury-watch.jpg",
+        name: "Men Luxury Watch",
+        priceCedis: "6000",
+        rating: {
+            stars: "3.5",
+            counts: "302",
+        },
+    },
+
+    {
+        id: "6b07d4e7-f540-454e-8a1e-363f25dbae7d",
+        image: "../images/products/women-white-dress-heels.jpg",
+        name: "Women Luxury Heels",
+        priceCedis: "25000",
+        rating: {
+            stars: "4.5",
+            counts: "610",
+        },
+    },
+
+    {
+        id: "a82c6bac-3067-4e68-a5ba-d827ac0be010",
+        image: "../images/products/mens-cream-brown-fashion-sneakers.jpg",
+        name: "Men Fashion Sneaker - Brown",
+        priceCedis: "14000",
+        rating: {
+            stars: "4",
+            counts: "309",
+        },
+    },
+
+    {
+        id: "e4f64a65-1377-42bc-89a5-e572d19252e2",
+        image: "../images/products/women-quilted-shoulder-handbag.jpg",
+        name: "Women Shoulder Handbag",
+        priceCedis: "15000",
+        rating: {
+            stars: "4",
+            counts: "203",
+        },
+    },
+
+    {
+        id: "b0f17cc5-8b40-4ca5-9142-b61fe3d98c85",
+        image: "../images/products/mens-white-red-retro-basketball-sneaker.jpg",
+        name: "Men basketball Sneaker- Red & white",
+        priceCedis: "12000",
+        rating: {
+            stars: "4.5",
+            counts: "307",
+        },
+    },
+
+    {
+        id: "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
+        image: "../images/products/womens-jean-collection.jpg",
+        name: "Women Jean collection",
+        priceCedis: "11000",
+        rating: {
+            stars: "4",
+            counts: "408",
+        },
+    },
+
+    {
+        id: "4f4fbcc2-4e72-45cc-935c-9e13d79cc57f",
+        image: "../images/products/men-white-air-bubble-designer-fashion-sneaker.jpg",
+        name: "Men Designer Sneaker - White",
+        priceCedis: "23000",
+        rating: {
+            stars: "4.5",
+            counts: "448",
+        },
+    },
+
+    {
+        id: "8b5a2ee1-6055-422a-a666-b34ba28b76d4",
+        image: "../images/products/men-sport-running-sneaker.jpg",
+        name: "Men Sport Running Sneaker",
+        priceCedis: "20000",
+        rating: {
+            stars: "4.5",
+            counts: "501",
+        },
+    },
+
+    {
+        id: "b86ddc8b-3501-4b17-9889-a3bad6fb585f",
+        image: "../images/products/women-floral-mini-handbag.jpg",
+        name: "Women Floral Mini Handbag",
+        priceCedis: "18000",
+        rating: {
+            stars: "4.5",
+            counts: "308",
+        },
+    },
+
+    {
+        id: "19c6a64a-5463-4d45-9af8-e41140a4100c",
+        image: "../images/products/mens-green-street-style-sneaker.jpg",
+        name: "Men Street Sneaker",
+        priceCedis: "9050",
+        rating: {
+            stars: "3.5",
+            counts: "178",
+        },
+    },
+
+    {
+        id: "d2785924-743d-49b3-8f03-ec258e640503",
+        image: "../images/products/men-black-white-patterned-sneaker.jpg",
+        name: "Men Patterned Sneaker - Black & White",
+        priceCedis: "16000",
+        rating: {
+            stars: "4.5",
+            counts: "328",
+        },
+    },
+
+    {
+        id: "1c079479-8586-494f-ab53-219325432536",
+        image: "../images/products/men-white-red-retro-basketball-sneakers.jpg",
+        name: "Men basketball Sneaker-White & red",
+        priceCedis: "10000",
+        rating: {
+            stars: "4",
+            counts: "209",
+        },
+    },
+
+    {
+        id: "4e37dd03-3b23-4bc6-9ff8-44e112a92c64",
+        image: "../images/products/men-white-platform-casual-sneaker.jpg",
+        name: "Men Casual Sneaker",
+        priceCedis: "9500",
+        rating: {
+            stars: "3.5",
+            counts: "167",
+        },
+    },
+
+    {
+        id: "4df68c27-fd59-4a6a-bbd1-e754ddb6d53c",
+        image: "../images/products/men-beige-and-white-urban-sneaker.jpg",
+        name: "Men Urban Sneaker - Beige",
+        priceCedis: "13080",
+        rating: {
+            stars: "4.5",
+            counts: "180",
+        },
+    },
+
+    {
+        id: "a93a101d-79ef-4cf3-a6cf-6dbe532a1b4a",
+        image: "../images/products/womens-jean-collection.jpg",
+        name: "Women Jean collection",
+        priceCedis: "11000",
+        rating: {
+            stars: "4",
+            counts: "408",
+        },
+    },
+
+    {
+        id: "a434b69f-1bc1-482d-9ce7-cd7f4a66ce8d",
+        image: "../images/products/men-green-white-patterned-sneaker.jpg",
+        name: "Men Patterned Sneaker - Green & White",
+        priceCedis: "11000",
+        rating: {
+            stars: "4.5",
+            counts: "206",
+        },
+    },
+
+    {
+        id: "0d7f9afa-2efe-4fd9-b0fd-ba5663e0a524",
+        image: "../images/products/towel.jpg",
+        name: "Luxury Towels - Variations",
+        priceCedis: "11000",
+        rating: {
+            stars: "4.5",
+            counts: "407",
+        },
+    },
+
+    {
+        id: "36c64692-677f-4f58-b5ec-0dc2cf109e27",
+        image: "../images/products/jacket.jpg",
+        name: "Winter Jacket - Black",
+        priceCedis: "11090",
+        rating: {
+            stars: "4.0",
+            counts: "256",
+        },
+    },
+];
