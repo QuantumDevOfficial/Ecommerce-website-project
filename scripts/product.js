b4dd14da9de28320e4548f9b5ad76a08e76596b1
Bedzo Promise <promisebedzo07@gmail.com>
2025-12-29 12:18:29 +0000

    Generate products with js and refine add to favorite button

--- scripts/product.js
+++ scripts/product.js
@@ -373,3 +373,81 @@ const products = [
         },
     },
 ];
+
+let productHTML = '';
+
+products.forEach((product) => {
+    productHTML += `
+        <div class="product-container">
+            <div class="product-image-container">
+                <img src="${product.image}" alt="..." class="product-image">
+            </div>
+            <div class="product-name">
+                ${product.name}
+            </div>
+            <div class="product-rating-container">
+                <img src="images/ratings/rating-${product.rating.stars * 10}.png" alt="Stars" class="product-rating-stars">
+                <div class="product-rating-counts">
+                    ${product.rating.counts}
+                </div>
+            </div>
+            <div class="product-price">Gh¢${(product.priceCedis / 100).toFixed(2)}</div>
+            
+            <div class="product-quantity-container">
+                <select name="quantity" class="quantity">
+                    <option selected value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                </select>
+
+                <button onclick="activation(event)" class="add-to-favarite">
+                    <img class="activated" src="images/icons/icons8-favorite-24 (2).png" alt="">
+                    <img class="deactivated" src="images/icons/icons8-favorite-24.png" alt="">
+                </button>
+            </div>
+
+            <div class="add-to-cart-alert">
+                <img src="images/icons/checkmark.png" alt="">
+                Added
+            </div>
+
+            <button class="add-to-cart-button">
+                <img src="images/icons/icon8-shopping-cart-24.png" alt="">
+                Add to Cart
+            </button>
+        </div>
+   `;
+});
+
+document.querySelector('.container-grid').innerHTML = productHTML;
+console.log(productHTML);
+
+// Shuffle Carousole
+
+const carousel = document.querySelectorAll(".carousel");
+let recent = 0;
+
+setInterval(()=>{
+    carousel[recent].classList.remove("active");
+    recent = (recent + 1) % carousel.length;
+    carousel[recent].classList.add("active");
+}, 4000);
+
+// Add to favorite
+
+function activation(e) {
+    const clickedButton = e.currentTarget;
+    const activated = clickedButton.querySelector('.activated');
+    const deactivated = clickedButton.querySelector('.deactivated');
+    activated.classList.add('deactivated');
+    activated.classList.remove('activated');
+    deactivated.classList.add('activated');
+    deactivated.classList.remove('deactivated');
+}
